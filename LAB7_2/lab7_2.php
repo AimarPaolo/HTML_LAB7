9aa74a2a5e8021fce00e57a3043fc68766d9fc79
Paolo Aimar <s297424@studenti.polito.it>
2024-05-07 10:23:34 +0200

    Esercizio 2) Si scrive prima un file in HTML e successivamente si crea un altro file php che crea un'altra pagina html utilizzando i dati ricevuti dal server

---
+++ LAB7_2/lab7_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Esercizio 7.2</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Paolo Aimar">
+    </head>
+    <body>
+        <h1>Testo che è stato ricevuto dal server: </h1>
+        <?php
+            if(isset($_REQUEST['testo']) && $_REQUEST['testo'] != ""){
+                $testo = $_REQUEST['testo'];
+                echo "<p>il testo inserito nel form è: $testo</p>";
+            }else{
+                echo "<p>DOVEVI INSERIRE UN VALORE NEL CAMPO DI TESTO</p>"
+            }
+        ?>
+    </body>
+</html>
